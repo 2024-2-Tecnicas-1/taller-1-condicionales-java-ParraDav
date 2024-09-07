dd831de2ea09417cce97a053c22f7df1f024f547
ParraDav <df.parra.07@gmail.com>
2024-09-07 17:35:04 -0500

    Orden
    Se añade código de ordenamiento de números

--- src/main/java/ejercicios/Ordenamiento.java
+++ src/main/java/ejercicios/Ordenamiento.java
@@ -1,7 +1,6 @@
-
 package ejercicios;
 
-import static ejercicios.SetDeTenis.evaluar;
+import java.time.Clock;
 import java.util.Scanner;
 
 /**
@@ -9,11 +8,33 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class Ordenamiento {
+
     public static String evaluar(int numero1, int numero2, int numero3, int numero4) {
         // TODO: Coloca aquí el código del ejercicio 5: Ordenamiento
-        return "";
+        int[] numeros = {numero1, numero2, numero3, numero4};
+        for (int i = 0; i < numeros.length; i++) {
+
+            for (int j = 0; j < numeros.length - 1; j++) {
+                int a = numeros[j];
+                int s = numeros[j + 1];
+                if (a > s) {
+                    numeros[j] = s;
+                    numeros[j + 1] = a;
+
+                }
+            }
+        }
+        StringBuilder k = new StringBuilder();
+        for (int i = 0; i < numeros.length; i++) {
+            k.append(numeros[i]);
+            if (i < numeros.length - 1) {
+                k.append(" ");
+            }
+
+        }
+        return k.toString();
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Número 1:");
@@ -24,7 +45,7 @@ public class Ordenamiento {
         int numero3 = lector.nextInt();
         System.out.print("Número 4:");
         int numero4 = lector.nextInt();
-        
+
         String respuesta = evaluar(numero1, numero2, numero3, numero4);
         System.out.println(respuesta);
     }
